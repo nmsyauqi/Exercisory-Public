addc0af2d11297d14a8e50229d5679deb3d4e40f
nmsyauqi <syauqimuhammad430@gmail.com>
2025-11-20 17:25:22 +0700

    opt

--- app/Livewire/Admin/ViewParticipantHistory.php
+++ app/Livewire/Admin/ViewParticipantHistory.php
@@ -8,6 +8,18 @@ use App\Models\User;
 class ViewParticipantHistory extends Component
 {
     public User $user;
+
+    public function mount(User $user)
+    {
+        // user dilihat harus bukan admin
+        if ($user->role === 'admin') {
+            // eror atau redirect
+            return redirect()->route('admin.users.index')
+                ->with('error', 'Tidak dapat melihat riwayat akun Administrator.');
+        }
+
+        $this->user = $user;
+    }
     public function render()
     {
         return view('livewire.admin.view-participant-history')
